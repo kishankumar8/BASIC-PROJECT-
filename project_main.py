70c398fa778ca13798718c07c777433c4ff7f0ae
kishankumar8 <kt2561060@gmail.com>
2025-09-10 20:33:04 +0530

    Add files via upload
    THIS IS MY FIRST BASIC PROJECT OF PYTHON .
    IN THIS I MAKE A GAME OF STONE ,PAPER , SCISSOR USING PYTHON.
    IN THIS I USE ( IF, ELIF, ELSE ) CONTROL STATEMENT .
    PLAY THIS GAME

---
+++ project_main.py
@@ -0,0 +1,28 @@
+#stone,paper,scissor game
+
+import random
+choices = ["stone","paper", "scissor"]
+user_choice = input(" enter stone,paper,or scissor: ").lower()
+computer_choice = random.choice(choices)
+print("computer_choice: ",computer_choice)
+
+#BASIC RULES FOR PLAYING THIS GAME
+
+"""
+STONE BEATS SCISSORS
+PAPER BEATS STONE
+SCISSORS BEATS PAPER
+
+"""
+
+if(user_choice == computer_choice):
+    print("match is tie")
+
+elif(user_choice == "stone" and computer_choice == "scissor"):
+    print("you win the match")
+
+elif(user_choice == "paper" and computer_choice == "stone" ):
+    print("you win the match")
+
+elif(user_choice == "scissor" and computer_choice == "paper"):
+    print("you win the match")
